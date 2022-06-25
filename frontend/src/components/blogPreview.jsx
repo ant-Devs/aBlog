40e08804475f8183318092362d9e60ef1c2039bf
David-Main <work.davidmainoo@gmail.com>
2022-06-25 13:30:10 +0000

    make home page responsive

--- frontend/src/components/blogPreview.jsx
+++ frontend/src/components/blogPreview.jsx
@@ -2,16 +2,16 @@
 export default function Blog({ date, views, title, image }) {
   return (
     <>
-      <div className="flex justify-bewteen gap-2 items-center min-w-[19rem] w-10/12 max-w-[25rem] h-32">
+      <div className="flex justify-bewteen gap-2 items-center min-w-[19rem] w-9/12 max-w-[25rem] md:max-w-[40rem] ">
         <div>
-          <h3 className="text-xl tracking-7  font-bold" style={{ "lineHeight": "1.1", "flex": "1 1 80%" }}>{title || "Title of the blogger goes here too again"}</h3>
-          <div className="flex text-sm text-neutral-700 gap-5">
+          <h3 className="text-xl md:text-4xl font-bold" style={{ "lineHeight": "1", "flex": "1 1 90%" }}>{title || "Title of the blogger goes here too again"}</h3>
+          <div className="flex md:mt-3 mt-2 text-sm md:text-xl text-neutral-700 gap-5">
             <p>{date || "date"}</p>
             <p>{views || "views"}</p>
           </div>
         </div>
 
-        <div className="bg-gray-400  h-full min-w-[8rem]" style={{ "flex": "1 1 80%" }}>
+        <div className="bg-gray-400 h-28 md:h-44 min-w-[8rem] max-w-[15rem]" style={{ "flex": "1 1 80%" }}>
           <img src={image} alt="pic" />
         </div>
       </div>
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,12 +1,12 @@
 export default function() {
   return (
     <>
-      <header className="bg-slate-800 text-gray-50 hero h-[43vh] min-h-[20rem] max-h-[22rem] flex flex-col gap-2 items-center justify-center">
+      <header className="bg-slate-800 text-gray-50 hero h-[43vh] md:h-[65vh] min-h-[20rem] max-h-[42rem] flex flex-col gap-2 md:gap-5 items-center justify-center">
 
-        <h1 className="text-5xl font-bold text-center">
+        <h1 className="text-5xl md:text-8xl font-bold text-center">
           a.Blog
         </h1>
-        <p className="text-sm font-semibold flex gap-3"><span> ----</span> Good reads for all needs <span> ----</span> </p>
+        <p className="text-sm md:text-2xl font-semibold flex gap-3"><span> ----</span> Good reads for all needs <span> ----</span> </p>
 
       </header>
     </>
